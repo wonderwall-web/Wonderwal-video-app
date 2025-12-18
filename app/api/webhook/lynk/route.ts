3bce0d61304b64edd0685d88688cdbd743fe188d
Coong <coong@Coongs-MacBook-Pro.local>
2025-12-18 09:07:01 +0700

    save sales to google sheet

--- app/api/webhook/lynk/route.ts
+++ app/api/webhook/lynk/route.ts
@@ -3,13 +3,8 @@ import { NextResponse } from "next/server"
 export async function POST(req: Request) {
   const { secret, email } = await req.json()
 
-  // PAKSA SECRET (BUKAN ENV)
   if (secret !== "lynk_rahasia_123") {
     return NextResponse.json({ ok: false }, { status: 401 })
-  }
-
-  if (!email) {
-    return NextResponse.json({ ok: false }, { status: 400 })
   }
 
   const license =
@@ -17,6 +12,16 @@ export async function POST(req: Request) {
     Math.random().toString(36).slice(2, 6).toUpperCase() +
     "-" +
     Math.random().toString(36).slice(2, 6).toUpperCase()
+
+  // 🔔 KIRIM KE GOOGLE SHEET
+  await fetch("https://script.google.com/u/0/home/projects/1NEZtUIDUD1fMeDsqMHhSc5svcJiNCbChxC1SYI6XFGlFAN93fcmpc83r/edit", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email,
+      license
+    })
+  })
 
   return NextResponse.json({
     ok: true,
