b557f5229c7bbb29075a6f55f4df48324c60c066
Coong <coong@Coongs-MacBook-Pro.local>
2025-12-18 13:55:43 +0700

    connect google sheet exec webhook

--- app/api/webhook/lynk/route.tsx
+++ app/api/webhook/lynk/route.tsx
@@ -3,21 +3,38 @@ import { NextResponse } from "next/server"
 export async function POST(req: Request) {
   const { secret, email } = await req.json()
 
+  // VALIDASI SECRET
   if (secret !== "lynk_rahasia_123") {
     return NextResponse.json({ ok: false }, { status: 401 })
   }
 
+  if (!email) {
+    return NextResponse.json({ ok: false }, { status: 400 })
+  }
+
+  // GENERATE LICENSE
   const license =
     "LIC-" +
     Math.random().toString(36).slice(2, 6).toUpperCase() +
     "-" +
     Math.random().toString(36).slice(2, 6).toUpperCase()
 
-  await fetch("https://script.google.com/macros/s/AKfycbyFgEt7dlKt4Q18zDYFsZkNraMN9v-_a61srkXAKr_nHPS-ldcKddnNsBuqkryzs8Zopw/exec", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, license })
-  })
+  // KIRIM KE GOOGLE SHEET (WEB APP /exec)
+  await fetch(
+    "https://script.google.com/macros/s/AKfycbwy2VMKvv86NnxI8xyWs516KKptJgfQD5DS43vApVlSnHCUA3-JmxIKnzpmftcOqIPmtg/exec",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email,
+        license
+      })
+    }
+  )
 
-  return NextResponse.json({ ok: true, email, license })
+  return NextResponse.json({
+    ok: true,
+    email,
+    license
+  })
 }
